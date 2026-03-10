6c2243d225bdbac6f198eeac62f106984483abdb
nbollinger50 <nbollinger50@gmail.com>
2026-03-09 23:04:10 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -16,21 +16,21 @@ time.sleep(0.05)
 st.title("Simple chat")
 # Initialize chat history
 if "messages" not in st.session_state:
-st.session_state.messages = []
+  st.session_state.messages = []
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
-with st.chat_message(message["role"]):
-st.markdown(message["content"])
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
 # Accept user input
 if prompt := st.chat_input("What is up?"):
 # Display user message in chat message container
-with st.chat_message("user"):
-st.markdown(prompt)
+  with st.chat_message("user"):
+    st.markdown(prompt)
 # Add user message to chat history
 st.session_state.messages.append({"role": "user", "content": prompt})
 # Display assistant response in chat message container
-with st.chat_message("assistant"):
-response = st.write_stream(response_generator())
+  with st.chat_message("assistant"):
+    response = st.write_stream(response_generator())
 # Add assistant response to chat history
 st.session_state.messages.append({"role": "assistant", "content": response})
 
