69d767182a2d53f49601dc0a7464c37dbc81aca3
nbollinger50 <nbollinger50@gmail.com>
2026-03-09 23:06:30 -0700

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -3,12 +3,12 @@ import random
 import time
 # Streamed response emulator
 def response_generator():
-response = random.choice(
-[
-"Hello there! How can I assist you today?",
-"Hi, human! Is there anything I can help you with?",
-"Do you need help?",
-]
+  response = random.choice(
+  [
+    "Hello there! How can I assist you today?",
+    "Hi, human! Is there anything I can help you with?",
+    "Do you need help?",
+  ]
 )
 for word in response.split():
 yield word + " "
